0118f25d43a31f7f84adc9b0aff5aa296f210a03
Di Wang <wangd7@student.unimelb.edu.au>
2018-04-26 16:27:24 +1000

    copy contents to clipboard

--- src/Card/index.js
+++ src/Card/index.js
@@ -13,7 +13,7 @@ export default class Card extends Component {
         return (
             <div className="card-container">
                 <div className="color-panel" style={{backgroundImage}}>
-                    <div className="clip-btn" onClick={this._onClick.bind(this)}>
+                    <div className="clip-btn" onClick={this._onClick.bind(this, backgroundImage)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                              stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                              className="feather feather-code">
@@ -38,7 +38,26 @@ export default class Card extends Component {
         })
     }
 
-    _onClick() {
+    _onClick(text) {
+        const content = `background-image: ${text};`;
+        if (window.clipboardData && window.clipboardData.setData) {
+            // IE specific code path to prevent textarea being shown while dialog is visible.
+            return window.clipboardData.setData("Text", content);
 
+        } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+            const textarea = document.createElement("textarea");
+            textarea.textContent = content;
+            textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+            } catch (e) {
+                console.warn("Copy to clipboard failed.", e);
+                return false;
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
     }
 }
