b2f32b13ac94e1513fc6366ec94624613b9860c1
Di Wang <wangd7@student.unimelb.edu.au>
2018-04-26 15:19:29 +1000

    add a value

--- src/values.js
+++ src/values.js
@@ -2,5 +2,6 @@ export default [
     'linear-gradient(45deg, #3a67b0, #3bd2f3)',
     'linear-gradient(45deg, #3e9fb5, #69d1cf)',
     'linear-gradient(45deg, #f791a7, #ffa26e)',
-    'linear-gradient(45deg, #aaafe3, #dd8ce8)'
+    'linear-gradient(45deg, #aaafe3, #dd8ce8)',
+    'linear-gradient(45deg, #509BFB, #3EE5FB)',
 ]
